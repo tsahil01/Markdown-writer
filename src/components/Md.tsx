5b02f3655b04cbdf3d86c0509261370502377f03
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-29 22:51:35 +0530

    final md

--- src/components/Md.tsx
+++ src/components/Md.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import 'highlight.js/styles/github.css';
 
@@ -11,25 +11,26 @@ export default function Md() {
     const [markdown, setMarkdown] = useState("# Hello");
 
     return (
-        <div className="flex flex-row gap-5 justify-between p-5">
+        <div className="flex md:flex-row flex-col gap-5 justify-between p-5">
 
-            <div className="border rounded-lg h-screen w-1/2">
+            <div className="border outline-none rounded-lg min-h-1/2 bg-zinc-800 block lg:w-1/2">
                 <textarea
-                    className="w-full h-full p-3 overflow-auto b-0" // Adjusted padding for textarea
+                    className="w-full min-h-1/2 p-3 resize-none outline-none bg-zinc-800 text-white"
+                    style={{ fontFamily: "inherit", fontSize: "16px" }}
                     onChange={(e) => setMarkdown(e.target.value)}
                 ></textarea>
             </div>
 
-            <div className="border rounded-lg w-1/2 p-4 h-full bg-white overflow-auto"> {/* Added overflow-auto for scrollability */}
-                <ReactMarkdown className="prose"
+            <div className="rounded-lg lg:w-1/2 min-h-1/2 overflow-auto outline-none">
+                <ReactMarkdown className="markdown w-full min-h-1/2 overflow-auto outline-none bg-zinc-800 p-4 text-white"
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeRaw]}
                     components={{
-                        code({ node, inline, className, children, ...props }: any) {
+                        code({ node, inline, className, children, ...props }) {
                             const match = /language-(\w+)/.exec(className || '');
 
                             return !inline && match ? (
-                                <SyntaxHighlighter style={dracula} PreTag="div" language={match[1]} {...props}>
+                                <SyntaxHighlighter className="rounded-lg mx-4" style={vscDarkPlus} PreTag="div" language={match[1]} {...props}>
                                     {String(children).replace(/\n$/, '')}
                                 </SyntaxHighlighter>
                             ) : (
